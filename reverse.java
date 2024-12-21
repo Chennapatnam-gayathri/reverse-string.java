bfe601a0e823c2a61698eebbb1825f834bd178f8
Chennapatnam-gayathri <152840303+Chennapatnam-gayathri@users.noreply.github.com>
2024-12-21 17:53:28 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,11 @@
+public class reverse {
+    public static void main(String[] args){
+        String s="gayathri";
+        String anw="";
+        for(int i=7 ; i>=0 ; i--){
+           anw = anw +s.charAt(i);
+        }
+        System.out.print(anw);
+    }
+    
+}
